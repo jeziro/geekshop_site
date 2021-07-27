2bf4de4f2ae9f66763e21ccbeefe1e2fe66eb7f3
jeziro <jeziro@gmail.com>
2021-07-27 14:42:05 +0300

    lesson 7 upd

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from mainapp.models import Product
+from django.utils.functional import cached_property
 
 
 
@@ -20,31 +21,37 @@ class Basket(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
-    
-    
+
     def _get_product_cost(self):
         "return cost of all products this type"
         return self.product.price * self.quantity
-    
+
     product_cost = property(_get_product_cost)
-    
-    
+
+    @cachedprorepty
+    def get_items_cached(self):
+        return  self.user.basket.select_related()
+
     def _get_total_quantity(self):
         "return total quantity for user"
-        _items = Basket.objects.filter(user=self.user)
-        _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
-        return _totalquantity
-        
-    total_quantity = property(_get_total_quantity)
-    
+        # _items = Basket.objects.filter(user=self.user)
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.quantity, _items)))
+        # _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
+        # return _totalquantity
+        #
+        # total_quantity = property(_get_total_quantity)
+        #
     
     def _get_total_cost(self):
         "return total cost for user"
-        _items = Basket.objects.filter(user=self.user)
-        _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
-        return _totalcost
-        
-    total_cost = property(_get_total_cost)
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.product_cost, _items)))
+        # _items = Basket.objects.filter(user=self.user)
+        # _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
+        # return _totalcost
+        #
+        # total_cost = property(_get_total_cost)
 
     @staticmethod
     def get_items(user):
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,9 +4,23 @@ from mainapp.models import ProductCategory, Product
 from basketapp.models import Basket
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
+from django.core.cache import cache
 
 
 JSON_PATH = 'mainapp/json'
+
+def get_links_menu():
+    if settings.LOW_CACHE:
+        key = 'links_menu'
+        links_menu = cache.get(key)
+        if links_menu is None:
+            links_menu = ProductCategory.objects.filter(is_active=True)
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.filter(is_active=True)
+
 
 
 def load_from_json(file_name):
@@ -52,7 +66,7 @@ def main(request):
 
 def products(request, pk=None, page=1):   
     title = 'продукты'
-    links_menu = ProductCategory.objects.filter(is_active=True)
+#    links_menu = ProductCategory.objects.filter(is_active=True)
     basket = get_basket(request.user)
            
     if pk:
@@ -76,7 +90,7 @@ def products(request, pk=None, page=1):
         
         content = {
             'title': title,
-            'links_menu': links_menu,
+            'links_menu': get_links_menu(),
             'category': category,
             'products': products_paginator,
             'basket': basket,
@@ -89,7 +103,7 @@ def products(request, pk=None, page=1):
     
     content = {
         'title': title,
-        'links_menu': links_menu, 
+        'links_menu': get_links_menu(),
         'hot_product': hot_product,
         'same_products': same_products,
         'basket': basket,
@@ -100,13 +114,13 @@ def products(request, pk=None, page=1):
     
 def product(request, pk):
     title = 'продукты'
-    links_menu = ProductCategory.objects.filter(is_active=True)
+#    links_menu = ProductCategory.objects.filter(is_active=True)
 
     product = get_object_or_404(Product, pk=pk)
     
     content = {
         'title': title, 
-        'links_menu': links_menu, 
+        'links_menu': get_links_menu(),
         'product': product, 
         'basket': get_basket(request.user),
     }
@@ -116,11 +130,11 @@ def product(request, pk):
 def contact(request):
     title = 'о нас'
     
-    #locations = load_from_json('contact_locations')
+    locations = load_from_json('contact_locations')
     
     content = {
         'title': title,
-        #'locations': locations,
+        'locations': locations,
         'basket': get_basket(request.user),
     }
     
