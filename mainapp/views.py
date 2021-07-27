2f0c7a0953b4b1528bac302ffca9efd12d7a3444
jeziro <jeziro@gmail.com>
2021-07-27 15:21:49 +0300

    lesson 7 upd

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,7 +5,8 @@ from basketapp.models import Basket
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.conf import settings
-from django.core.cache import cache, cache_page
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 
 
 JSON_PATH = 'mainapp/json'
