d0c226a879a8e9bd484277244fb38e0df181da02
jeziro <jeziro@gmail.com>
2021-07-27 17:06:26 +0300

    lesson 7 upd

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -1,15 +1,16 @@
 # from django.conf.urls import url
 
 import basketapp.views as basketapp
-from django.urls import path
+from django.urls import re_path
 
 app_name="basketapp"
 
 urlpatterns = [
-    path('', basketapp.basket, name='list'),
+    path('', basketapp.basket, name='view'),
     path('add/<int:pk>/', basketapp.basket_add, name='add'),
     path('remove/<int:pk>/', basketapp.basket_remove, name='remove'),
     path('edit/<int:pk>/<int:quantity>/', basketapp.basket_edit, name='edit')
 
 ]
+
     
