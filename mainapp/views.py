e7cd36745e1f6c977b0ed8ad3e264383c29f4b60
jeziro <jeziro@gmail.com>
2021-07-27 17:58:41 +0300

    lesson 7 upd

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,6 +7,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.conf import settings
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page, never_cache
+from django.utils.functional import cached_property
 
 
 JSON_PATH = 'mainapp/json'
